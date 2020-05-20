83c27162c9e189e4c358987ad435f5ddc256a2f4
root <root@3360d88f8f13>
2020-05-20 23:12:09 +0000

    finished

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,23 +2,52 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot S= new Robot();
+	    //2. Make the robot draw a shape (this will take more than one line of code)
+		for (int i = 0; i <4; i++) {
+	S.penDown();
+		
+S.move(200);
+
+S.turn(90);
 		//3. Set the pen width to 10
+S.setPenWidth(10);
+		
 		//4. Ask the user what color pen they would like the robot to draw with
+String pen= JOptionPane.showInputDialog("What color pen would you like the robot to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
+if (pen.equals("Red")){
+	S.setPenColor(Color.red);
+	}
+	if(pen.equals("Green")) {
+		S.setPenColor(Color.green);
+	}
+		if(pen.equals("Blue")) {
+			S.setPenColor(Color.blue);
+			
+		}
+		else { S.setRandomPenColor();
+		}
+		}
+	
+
         //6. If the user doesn't enter anything, choose a random color
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
 		
 
-
+	}
 
 	}
-}
+
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,16 +3,28 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String cat=JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
+				int catasint=Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if(catasint>3) {
+			JOptionPane.showMessageDialog(null, "You are a crazy cat lady.");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
+		if(catasint>0&&catasint<3) {
+			playVideo("https://youtu.be/XyNlqQId-nk");
+		}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(catasint==0) {
+			playVideo("https://youtu.be/ZJT9CeEhM10");
+		}
 		
 		
 		
